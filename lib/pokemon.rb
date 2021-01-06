1f77df39a0ea246f7a9da27dac0e6fd680533969
Stephen <stephenjsweets@gmail.com>
2021-01-06 09:56:51 -0500

    commit tree a2029a84e0983e46fcb57e9b123f383921424e7a

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,34 @@
 class Pokemon
+
+    attr_accessor :id, :name, :type, :db
+
+    def initialize(id:, name:, type:, db:)
+        self.id = id
+        self.name = name
+        self.type = type
+        self.db = db
+    end
+
+    def self.save(name, type, db)
+        sql = <<-SQL
+            INSERT INTO pokemon (name, type)
+            VALUES (?, ?)
+        SQL
+
+        db.execute(sql, name, type)
+    end
+
+    def self.find(id, db)
+        sql = <<-SQL
+            SELECT *
+            FROM pokemon
+            WHERE id = ?
+            LIMIT 1
+        SQL
+
+        db.execute(sql, id).map do |row|
+            self.new(id: row[0], name: row[1], type: row[2], db: db)
+        end.first
+    end
+
 end
